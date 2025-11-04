989101302c58bceab5aa43d949e09c9b2916e39c
Kazuma Arino <hogeika2@gmail.com>
2025-11-04 14:51:28 +0900

    Update script for apple silicon.

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-dotnet publish -c release -r osx-x64 --self-contained false /p:PublishSingleFile=true /p:IncludeNativeLibrariesForSelfExtract=true /p:IncludeAllContentForSelfExtract=true
-cp bin/release/net9.0/osx-x64/publish/htmnix ~/bin
+# dotnet publish -c release -r osx-x64 --self-contained false /p:PublishSingleFile=true /p:IncludeNativeLibrariesForSelfExtract=true /p:IncludeAllContentForSelfExtract=true
+dotnet publish -c release -r osx-arm64 --self-contained false /p:PublishSingleFile=true /p:IncludeAllContentForSelfExtract=true
+cp bin/release/net9.0/osx-arm64/publish/htmnix ~/bin
